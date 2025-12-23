f370a31c7cba13ccac9292a5bc1c1b201dc2e8b9
Paulo Rocha <124544730+opaulorocha@users.noreply.github.com>
2025-12-22 23:54:35 -0300

    Add files via upload

---
+++ desafio0037.py
@@ -0,0 +1,12 @@
+# Programa para aprovar um emprestimo bancario para compra de uma casa
+
+valor_casa = float(input("Qual eh o valor da casa? "))
+salario = float(input("Quanto eh o seu salario mensal? "))
+anos = int(input("Em quantos anos pretende pagar? "))
+prest_mensal = (valor_casa/anos)/12
+print("A sua prestacao mensal sera de {:.2f} reais".format(prest_mensal))
+
+if prest_mensal < (salario*0.30):
+    print("O seu emprestimo foi aprovado!")
+else:
+    print("O seu emprestimo nao foi aprovado!")
---
+++ desafio0038.py
@@ -0,0 +1,10 @@
+# Programa que converte um numero inteiro para binario, octal ou hexadecimal
+num = int(input("Digite um numero: "))
+base = int(input("Qual base de convesao vc pretende converter o numero {}?".format(num)))
+
+if base == 1:
+
+elif base == 2:
+
+elif base == 3:
+    
